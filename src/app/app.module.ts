6b11e437ca5b8ebe90332357341acc011b1aa563
YuTaek <yutaek.hwang@gmail.com>
2022-04-17 22:08:22 -0400

    up to end of Step 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   // 2 way binding
   declarations: [
-    AppComponent,HeroesComponent
+    AppComponent,HeroesComponent, HeroDetailComponent
   ],
   imports: [
     BrowserModule,FormsModule
