053316cca21f955208a0825829fd3a7ec4ec699c
YuTaek <yutaek.hwang@gmail.com>
2022-04-17 22:52:19 -0400

    up to end of Step 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   // 2 way binding
   declarations: [
-    AppComponent,HeroesComponent, HeroDetailComponent
+    AppComponent,HeroesComponent, HeroDetailComponent, MessagesComponent
   ],
   imports: [
     BrowserModule,FormsModule
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 import { HEROES } from '../mock-heroes';
 
 // decorator function that specifies the Angular metadata for the component.
@@ -11,19 +14,26 @@ import { HEROES } from '../mock-heroes';
 // Always export the component class so you can import it elsewhere … like in the AppModule.
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-}
-  
-
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   // a lifecycle hook. Angular calls ngOnInit() shortly after creating a component. 
   // It's a good place to put initialization logic.
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  // waits for observable to emit heroes from server, which can happen anytime. now async
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
