1670b65e1a3965d077a0ce6638180cfdcf1638fe
YuTaek <yutaek.hwang@gmail.com>
2022-05-31 17:56:47 -0400

    up to end of Step 5

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,27 +13,16 @@ import { HEROES } from '../mock-heroes';
 })
 // Always export the component class so you can import it elsewhere … like in the AppModule.
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
-  // a lifecycle hook. Angular calls ngOnInit() shortly after creating a component. 
-  // It's a good place to put initialization logic.
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
-  // waits for observable to emit heroes from server, which can happen anytime. now async
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
-
 }
